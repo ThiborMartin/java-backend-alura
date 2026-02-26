1e7210149700347ba6bb6f84662f74d987c013b0
Thibor Martin <thibormartin28@outlook.com>
2026-02-26 10:36:52 -0300

    feat(aula-1): implementa item 1 do desafio 2

---
+++ nivel1/01-java-basico/desafio-aula-1/demo/src/main/java/br/com/alura/aula1/desafio2/Casting.java
@@ -0,0 +1,10 @@
+package br.com.alura.aula1.desafio2;
+
+public class Casting {
+    public static void main(String[] args) {
+        //Item 1 do desafio:
+        double numero1 = 7.7;
+        double numero2 = 5.9;
+        System.out.println("A média de %.2f e %.2f é = %.2f".formatted(numero1, numero2, (numero1+numero2)/2));
+    }
+}
