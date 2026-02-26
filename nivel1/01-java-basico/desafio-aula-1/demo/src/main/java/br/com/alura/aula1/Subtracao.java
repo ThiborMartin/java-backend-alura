bbfe60825e0c9ed2d8e5c4f5c096264d8ab025c6
Thibor Martin <thibormartin28@outlook.com>
2026-02-26 00:33:38 -0300

    feat(aula-1): implementa item 5 do desafio 1

---
+++ nivel1/01-java-basico/desafio-aula-1/demo/src/main/java/br/com/alura/aula1/Subtracao.java
@@ -0,0 +1,10 @@
+package br.com.alura.aula1;
+
+public class Subtracao {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 5;
+
+        System.out.println("O resultado da soma de %d - %d = %d".formatted(numero1, numero2, (numero1 - numero2)));
+    }
+}
