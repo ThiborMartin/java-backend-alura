2275550d20ca63572c247936143b7ee0d57b9080
Thibor Martin <thibormartin28@outlook.com>
2026-02-26 00:27:23 -0300

    feat(aula-1): implementa item 3 do desafio 1

---
+++ nivel1/01-java-basico/desafio-aula-1/demo/src/main/java/br/com/alura/aula1/Estudos.java
@@ -0,0 +1,9 @@
+package br.com.alura.aula1;
+
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("Anotações de estudos: ");
+        System.out.println("Java é uma plataforma completa de programação;");
+        System.out.println("O código é executado em máquina virtual (JVM)");
+    }
+}
